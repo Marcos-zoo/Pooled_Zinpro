e8daf9fffef3725e6adf48487c60bad6b7e15f49
Marcos-zoo <62188418+Marcos-zoo@users.noreply.github.com>
2024-07-16 23:09:47 -0300

    Add files via upload

--- age_europa_pooled_cumulative.py
+++ age_europa_pooled_cumulative.py
@@ -77,7 +77,8 @@ def zinc_chrome_cumulative():
         )
 
         # Use the custom CSS class to center the subheader
-        st.markdown('<h2 class="center-text">Cumulative performance 1 to 42 days</h2>', unsafe_allow_html=True)
+        st.markdown('<h2 class="center-text">Cumulative performance </h2>', unsafe_allow_html=True)
+        st.markdown('<h2 class="center-text">1 to 42 days</h2>', unsafe_allow_html=True)
 
         # Map treatment codes to names
         df3['TR'] = df3['TR'].map({1: 'IM', 2: 'Availa-iso'})
